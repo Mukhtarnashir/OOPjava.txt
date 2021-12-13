f61bf87df7a18f560a6d5860c4467cfb622fe931
Mukhtarnashir <96013248+Mukhtarnashir@users.noreply.github.com>
2021-12-13 23:49:16 +0700

    field-oop.java

--- PersonApp.java
+++ PersonApp.java
@@ -1,16 +1,25 @@
 public class PersonApp {
 public static void main(String[] args) {
 
-    var person1 = new Person();
-    Person person2 = new Person();
+var person1 = new Person();
+person1.name = "Nashir";
+person1.address = "Losari";
+// person1.country = "TEGAL";//
 
-    
-    Person person3;
-    person3 = new Person();
+System.out.println(person1.name);
+System.out.println(person1.address);
+System.out.println(person1.country);
 
-    System.out.println(person1);
-    System.out.println(person2);
-    System.out.println(person3);
+
+
+Person person2 = new Person();
+
+Person person3;
+person3 = new Person();
+
+System.out.println(person2);
+System.out.println(person3);
+
     
 }
     
